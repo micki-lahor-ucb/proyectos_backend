0016f7954b58f9feaba980277313c9d8579dc58a
MickiLahor <micki.lahor@gmail.com>
2026-01-19 22:05:11 -0400

    ci: fix format and lint errors - demonstrate CI enforcement

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -36,11 +36,4 @@ export class AppController {
       );
     }
   }
-  
-  // Este comentario tiene formato incorrecto y una variable no usada para romper el CI
-  @Get('test')
-  testEndpoint( unusedParam: string ) {
-    const unusedVar = 'esto rompe el lint';
-    return 'test';
-  }
 }
